7d787b94e9dcc91f26611b0df01c8635bb5a1c8d
JonFriend <jonfriend@JonFriends-MBP.localdomain>
2023-03-06 21:02:29 -0600

    rickshaw home, gallery and page layout built

---
+++ client/src/Components/Verticals/Rickshaws.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {Container, Row, Col, Card} from 'react-bootstrap';
+
+const Rickshaws = (props) => {
+
+    return (
+        <>
+            <Container fluid className="p-0 mt-5" >
+                <Row style={{height: "100vh"}} className="p-0 m-0">
+
+                    <Col sm={0} lg={2} className="d-none d-lg-block p-0 m-0"></Col>
+
+                    <Col sm={12} lg={8} className="bg-light p-0 m-0">
+                        <Card className="m-3 p-3">
+
+                            <Row>
+                                <Col className="d-flex flex-wrap justify-content-center">
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/bigyellow.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/blu.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/brown.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/desert.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/garcia.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/grn.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/ian.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/redroof.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+
+                                    <Card className="text-center m-3 border-0" style={{width: "18rem",  height: "25rem"}}>
+                                        <Card.Body className="p-2">
+                                            <Card.Img fluid src='../Assets/Images/yellow.png'  />
+                                        </Card.Body>
+                                    </Card>
+
+                                </Col>
+                            </Row>
+                        </Card>
+
+                    </Col>
+
+                    <Col sm={0} lg={2} className="d-none d-lg-block p-0 m-0"></Col>
+
+                </Row>
+            </Container>
+        </>
+
+    ) // end return
+} // end const
+
+export default Rickshaws;
